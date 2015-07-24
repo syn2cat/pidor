5b949e9d8232d00cad14c76d1e1fbdd55d15987a
Level2 Pidor <sysops@syn2cat.lu>
2015-07-24 21:10:06 +0200

    only switch off projector if there is no signal or if it shows demo videos

--- scripts/beamerdetect.sh
+++ scripts/beamerdetect.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 if [ "$1" = "off" ]
 then
+  (
   projip=$(arp -an|awk -F'[()]' '/00:50:41:79:d1:34/{print $2}')
-  ssh pi@doorbuzz 'doorbuzz/projectionscreen.sh up'
-  wget -qO - 'http://'"$projip"'/tgi/return.tgi?command=2a3102fd0660'
+  signalsource="$(wget -qO - 'http://'"$projip"'/tgi/return.tgi?query=info' |awk -F'[<>]' '/<info>/{print substr($3,33,2)}')"
+  if [ "$signalsource" = "00" ] || [ "$signalsource" = "15" ]
+  then
+    ssh pi@doorbuzz 'doorbuzz/projectionscreen.sh up'
+    echo "wget http://$projip/tgi/return.tgi?command=2a3102fd0660"
+    wget -qO - 'http://'"$projip"'/tgi/return.tgi?command=2a3102fd0660' 2>&1 
+    echo $? 
+  else
+    echo "not disabling projection because source is at $signalsource" 
+  fi
+  ) | logger -t "$(basename $0) $$"
   exit
 fi
 prevstatus="unknown"
