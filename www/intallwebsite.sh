61a55181f0f38e3a824f2162bd0ad96eb1a46d91
pidor <pidor@level2.lu>
2017-03-13 22:26:45 +0000

    migrate from inittab to systemd

--- www/intallwebsite.sh
+++ www/intallwebsite.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp -pr www/htdocs/* /var/www/
-cp -pr www/cgi-bin/* /usr/lib/cgi-bin/*
+cp -pr htdocs/* /var/www/
+cp -pr cgi-bin/* /usr/lib/cgi-bin/
 cp -p etc/* /etc/apache2/sites-available
 a2ensite lights.level2.lu.conf
